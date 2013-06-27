312466d4dc5eb22b9632fec01b5fdd854307075e
Apoorv <sathyamgit@gmail.com>
2013-06-27 16:34:58 +0530

    commited by apoorv

--- src/com/olabsys/apoorv/apoorv1.java
+++
@@ -1,9 +0,0 @@
-package com.olabsys.apoorv;
-
-public class apoorv1 {
-	public static void main(String args[])
-	{
-		System.out.println("code.....");
-	}
-
-}
---
+++ src/com/olabsys/test/Sample2.java
@@ -0,0 +1,15 @@
+package com.olabsys.test;
+
+public class Sample2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("file created ..... by apoorv");
+
+	}
+
+}
